dab9065d465cac326d71d4e1af6301e9c11562c6
YuvCarl <carldrake969@gmail.com>
2022-09-01 12:00:19 +0100

    updated

--- app.js
+++ app.js
@@ -4,21 +4,22 @@ let closeNav = document.getElementById('close');
 
 openNav.addEventListener('click', function(){
     let timeIn = setTimeout(function(){
-        document.getElementById('slide').style.display = 'flex';
+        document.getElementById('slide').style.width = '60%';
+        document.getElementById('slide').style.paddingLeft = '10px';
         document.getElementById('close').style.display = 'block';
         document.getElementById('conn').style.display = 'block';
         document.getElementById('container').style.backgroundColor = 'dark';
-    }, 300)
+    }, 200)
 
 }) 
 
 closeNav.addEventListener('click', function(){
     let timeIn = setTimeout(function(){
-        document.getElementById('slide').style.display = 'none';
-        document.getElementById('slide').style.transition = '1000ms';
+        document.getElementById('slide').style.width = '0%';
+        document.getElementById('slide').style.paddingLeft = '0px';
         document.getElementById('close').style.display = 'none';
         document.getElementById('container').style.backgroundColor = '';
         document.getElementById('conn').style.display = 'none';
-    }, 300)
+    }, 200)
 
 }) 
